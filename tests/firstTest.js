621f0f2e5a819cdf1f71aa2571142304e5864b4d
Derek Jackson <derek.jackson@wdapl008.lan>
2022-02-07 12:58:52 -0500

    initial commit

---
+++ tests/firstTest.js
@@ -0,0 +1,12 @@
+const {Builder} = require('selenium-webdriver');
+
+
+async function example(){
+    // launch browser
+
+    // navigate to application
+
+    // add a todo
+
+    // close browser
+}
