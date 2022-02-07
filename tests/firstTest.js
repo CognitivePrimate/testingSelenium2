cad8511c339f719adf3cd97f725f79af82ceae68
Derek Jackson <derek.jackson@wdapl008.lan>
2022-02-07 14:56:30 -0500

    selenium assertion test with nodejs moving on to chai next

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,12 +1,27 @@
-const {Builder} = require('selenium-webdriver');
+require('chromedriver');
+const {Builder, By, Key} = require('selenium-webdriver');
+const assert = require('assert');
 
 
 async function example(){
     // launch browser
-
+    let driver = await new Builder().forBrowser('chrome').build();
     // navigate to application
-
+    await driver.get('https://lambdatest.github.io/sample-todo-app')
+    
     // add a todo
+    await driver.findElement(By.id('sampletodotext')).sendKeys('Learn Selenium', Key.RETURN);
 
-    // close browser
+    // assert
+    // finds last element in ul of li via its xpath '//li[last()]', grabs value and then returns into todoText variable
+    // actual xpath = /html/body/div/div/div/ul/li[INDEX GOES HERE]
+    let todoText = await driver.findElement(By.xpath('//li[last()]')).getText().then((value) => {
+        return value;
+    });
+
+    assert.strictEqual(todoText, 'Learn Selenium')
+
+    // close browser ---- COULD ALSO RUN AS 'AFTERALL()' function??
+    // await driver.quit();
 }
+example();
