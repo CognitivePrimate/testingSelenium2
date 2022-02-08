1f5a2262ea29b1dca9a64a44db2541f888df3eef
Derek Jackson <derek.jackson@wdapl008.lan>
2022-02-08 12:55:40 -0500

    added mochawesome for test reporting and changed default directory and filename

--- test/secondTest.js
+++ test/secondTest.js
@@ -31,6 +31,36 @@ describe("add another todo tests", () => {
     todoText.should.equal("Learn Selenium");
 
     // close browser ---- COULD ALSO RUN AS 'AFTERALL()' function??
+    await driver.quit();
+  });
+
+  it("successfully adds a THIRD todo to the application", async () => {
+    // launch browser
+    let driver = await new Builder().forBrowser("chrome").build();
+    // navigate to application
+    await driver.get("https://lambdatest.github.io/sample-todo-app");
+
+    // add a todo
+    await driver
+      .findElement(By.id("sampletodotext"))
+      .sendKeys("Learn Selenium", Key.RETURN);
+
+    // assert
+    // finds last element in ul of li via its xpath '//li[last()]', grabs value and then returns into todoText variable
+    // actual xpath = /html/body/div/div/div/ul/li[INDEX GOES HERE]
+    let todoText = await driver
+      .findElement(By.xpath("//li[last()]"))
+      .getText()
+      .then((value) => {
+        return value;
+      });
+
+    // assert using chai
+    todoText.should.equal("Learn your job");
+
+    // close browser ---- COULD ALSO RUN AS 'AFTERALL()' function??
     // await driver.quit();
   });
+
+  
 });
